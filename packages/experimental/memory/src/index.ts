2663f7096d6eb67e5c2ccaceda89b3b202c6c671
Artemedi <artemedi@users.noreply.github.com>
2026-08-26 19:41:50 +0300

    api: export remote memory contract

--- packages/experimental/memory/src/index.ts
+++ packages/experimental/memory/src/index.ts
@@ -15,6 +15,8 @@ function MemoryId(id: string): MemoryId {
 }
 
 export type * from './types.ts'
+export { normalizeOpenVikingRecords, normalizeTencentDbRecords } from './remote-contract.ts'
+export type { OpenVikingDepth, OpenVikingRecord, RemoteMemoryCitation, RemoteMemoryProvider, RemoteMemorySearchRequest, TencentDbRecord } from './remote-contract.ts'
 
 /** Maximum citations one request can return. */
 export const MAX_MEMORY_HITS = 20
